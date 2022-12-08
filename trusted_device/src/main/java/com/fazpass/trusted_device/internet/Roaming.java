de5a955e5eb003c6e7b01a801742989bad43b561
SRW <sulthanrayhan2009@gmail.com>
2022-12-08 18:39:20 +0700

    Update Roaming.java

--- trusted_device/src/main/java/com/fazpass/trusted_device/internet/Roaming.java
+++ trusted_device/src/main/java/com/fazpass/trusted_device/internet/Roaming.java
@@ -13,10 +13,10 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class Roaming {
 
     public static UseCase start(String baseUrl){
-        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        /*HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
         interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
-        //OkHttpClient client = new OkHttpClient.Builder().build();
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();*/
+        OkHttpClient client = new OkHttpClient.Builder().build();
         Gson gson = new GsonBuilder()
                 .setLenient()
                 .create();
